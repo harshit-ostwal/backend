89a5876ffc6237c204127f99d91d1c1d15978d13
Harshit Jain <harshitostwal1234@gmail.com>
2025-03-07 13:57:18 +0530

    Add CORS and cookie-parser middleware, implement error handling, and create utility classes for API responses and errors

--- src/index.js
+++ src/index.js
@@ -1,35 +1,17 @@
-// require('dotenv').config({ path: './env' })
 import dotenv from "dotenv";
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js"
 import connectDB from "./db/index.js"
+import { app } from "./app.js";
 
 dotenv.config({
     path: './ env'
 })
 
 connectDB()
-
-
-// Method 1
-/*
-import express from "express";
-const app = express();
-
-(async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-
-        app.on("error", () => {
-            console.error("Error: ", error);
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at PORT: ${process.env.PORT}`);
         })
-
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on PORT ${process.env.PORT}`);
-        })
-    } catch (error) {
-        console.error("Error: ", error);
-        throw error
-    }
-})()
-    */
+    })
+    .catch((err) => {
+        console.error("MongoDB Connection Failed: ", error)
+    })
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+// Method - 1
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+// Method - 2
+// const asyncHandler = () => { }
+// const asyncHandler = (func) => () => { }
+// const asyncHandler = (func) => async () => { }
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+
+
+export { asyncHandler }
